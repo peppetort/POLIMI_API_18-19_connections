a082b31f557dce13703c2c63d2eb06505ae02acf
Giuseppe Tortorelli <peppetort@gmail.com>
2019-06-03 15:33:51 +0200

    Optimization and bux fix

--- main.c
+++ main.c
@@ -3,65 +3,49 @@
 #include <malloc.h>
 #include <string.h>
 
-typedef struct rel_ent{
-    char *id_org;
-    struct rel_ent *next;
-}t_rel_ent;
+// STRUCTURES
 
-typedef struct rel{
+typedef struct orig_rel t_orig;
+typedef struct rel t_rel;
+typedef struct entity t_ent;
+
+struct orig_rel{
+    t_ent *org_pnt;
+    struct orig_rel *next;
+    struct orig_rel *prev;
+};
+
+struct rel{
     char *id_rel;
-    int pointed_count; //numero di relazioni del tipo id_rel ricevute
-    t_rel_ent *ent; //lista di persone di origine della relazione id_rel
+    int counter;
+    t_orig *orig_list;
+    t_orig **last_orig_node;
     struct rel *next;
-}t_rel;
+    struct rel *prev;
+};
 
-typedef struct entity{
+struct entity {
     char *id_ent;
-    t_rel *relation_type;
+    t_rel *rel_list;
+    t_rel **last_rel_node;
     struct entity *next;
-}t_ent;
+    struct entity *prev;
+};
 
 // GLOBAL VARIABLES
+
 t_ent **last_ent_node = NULL;
-int is_ent_start = 1;
-int is_rel_end = 1;
-int is_rel_ent_end = 1;
 
 // UTILS FUNCTIONS
 
-void print_ent(t_ent *entity_list){
-    t_ent *pnt = entity_list;
-    printf("O- ");
-    if(pnt != NULL){
-        do{
-            if(pnt->next != NULL) {
-                printf("%s -> ", pnt->id_ent);
-            } else{
-                printf("%s ", pnt->id_ent);
-            }
-            pnt = pnt->next;
-        }while (pnt != NULL);
-    }
-    printf("-|");
-}
-
-
-// controlla se un elemento è nella lista delle entità: se c'è restituisce il puntatore al nodo precedente altrimenti NULL
-t_ent *is_in_ent(char *id_ent, t_ent *entity_list){
-    t_ent *pnt, *prev_pnt;
-    if(entity_list != NULL){
-        pnt = entity_list;
-        prev_pnt = pnt;
+t_ent *is_in_ent(char *id_ent, t_ent *ent_list){
+    if(ent_list != NULL){
+        t_ent *pnt;
+        pnt = ent_list;
         do {
             if (strcmp(pnt->id_ent, id_ent) == 0) {
-                if(pnt == prev_pnt){
-                   is_ent_start = 1;
-                } else{
-                    is_ent_start = 0;
-                }
-                return prev_pnt;
+                return pnt;
             }
-            prev_pnt = pnt;
             pnt = pnt->next;
         }while (pnt != NULL);
     }
@@ -69,171 +53,175 @@ t_ent *is_in_ent(char *id_ent, t_ent *entity_list){
 }
 
 t_rel *is_in_rel(char *id_rel, t_rel *rel_list){
-    t_rel *pnt = rel_list;
-    t_rel *prev_pnt = NULL;
-    do{
-        if(strcmp(pnt->id_rel, id_rel) == 0){
-            is_rel_end = 1;
-            return pnt;
-        }
-        prev_pnt = pnt;
-        pnt = pnt->next;
-    }while (pnt != NULL);
-    is_rel_end = 0;
-    return prev_pnt;
-}
-
-t_rel_ent *is_in_rel_ent(char *id_orig, t_rel_ent *rel_ent_list){
-    t_rel_ent *pnt = rel_ent_list;
-    t_rel_ent *prev_pnt = rel_ent_list;
+    if(rel_list != NULL){
+        t_rel *pnt = rel_list;
         do{
-            if(strcmp(pnt->id_org, id_orig) == 0){
-                is_rel_ent_end = 1;
+            if(strcmp(pnt->id_rel, id_rel) == 0){
                 return pnt;
             }
-            prev_pnt = pnt;
             pnt = pnt->next;
         }while (pnt != NULL);
-        is_rel_ent_end = 0;
-        return prev_pnt;
+    }
+    return NULL;
 }
 
-void print_all_rel(char *ent, t_ent *entuty_list){
-    printf("RELAZIONI PUNTANTI %s\n", ent);
-    t_ent *pnt = is_in_ent(ent, entuty_list)->next;
-    t_rel *relations_list = pnt->relation_type;
-    printf("Tipo di relazione:\n");
-    do{
-        printf("  @%s\n", relations_list->id_rel);
-        printf("    Riferiment: %d\n", relations_list->pointed_count);
-        printf("    Origini:\n");
-        t_rel_ent *origin_list = relations_list->ent;
+ t_orig *is_in_origin(char *id_orig, t_orig *origin_list){
+    if(origin_list != NULL){
+      t_orig *pnt = origin_list;
+      do{
+          if(strcmp(pnt->org_pnt->id_ent, id_orig) == 0){
+              return pnt;
+          }
+          pnt = pnt->next;
+      }while (pnt != NULL);
+    }
+    return NULL;
+}
+
+void print_ent(t_ent *entity_list){
+    t_ent *pnt = entity_list;
+    printf("O<- ");
+    if(pnt != NULL){
         do{
-            printf("      - %s\n", origin_list->id_org);
+            if(pnt->next != NULL) {
+                printf("%s <-> ", pnt->id_ent);
+            } else{
+                printf("%s ", pnt->id_ent);
+            }
+            pnt = pnt->next;
+        }while (pnt != NULL);
+    }
+    printf("->|\n");
+}
+
+void print_all_rel(char *ent, t_ent *entity_list){
+    printf("RELAZIONI PUNTANTI %s\n", ent);
+    t_ent *result = is_in_ent(ent, entity_list);
+    t_ent *pnt = result;
+    t_rel *relation_list = pnt->rel_list;
+    printf("Tipo di relazione\n");
+    do{
+        printf("   @%s\n", relation_list->id_rel);
+        printf("      Riferimenti: %d\n", relation_list->counter);
+        printf("      Origini:\n");
+        t_orig *origin_list = relation_list->orig_list;
+        do{
+            printf("         - %s\n", origin_list->org_pnt->id_ent);
             origin_list = origin_list->next;
         }while (origin_list != NULL);
-        relations_list = relations_list->next;
-    }while (relations_list != NULL);
+        relation_list = relation_list->next;
+    }while (relation_list != NULL);
+    free(result);
 }
 
 
 // REQUIRED FUNCTIONS
 
 void add_ent(char *id_ent, t_ent **entity_list){
-    if(is_in_ent(id_ent, *entity_list) == NULL){
-        t_ent *new_node = (t_ent *)malloc(sizeof(t_ent));
+    t_ent *result = is_in_ent(id_ent, *entity_list);
+    if(result == NULL && last_ent_node == NULL) {
+        t_ent *new_node = (t_ent *) malloc(sizeof(t_ent));
         new_node->id_ent = id_ent;
-        new_node->relation_type = NULL;
+        new_node->rel_list = NULL;
+        new_node->last_rel_node = NULL;
         new_node->next = NULL;
-
-        if(*last_ent_node != NULL){
-            (*last_ent_node)->next = new_node;
-            last_ent_node = &((*last_ent_node)->next);
-        } else{
-            *entity_list = new_node;
-            last_ent_node = entity_list;
-        }
+        new_node->prev = NULL;
+        *entity_list = new_node;
+        last_ent_node = entity_list;
+    } else if(result == NULL && last_ent_node != NULL){
+        t_ent *new_node = (t_ent *) malloc(sizeof(t_ent));
+        new_node->id_ent = id_ent;
+        new_node->rel_list = NULL;
+        new_node->last_rel_node = NULL;
+        new_node->next = NULL;
+        new_node->prev = *last_ent_node;
+        (*last_ent_node)->next = new_node;
+        last_ent_node = &(*last_ent_node)->next;
     }
 }
 
 void del_ent(char *id_ent, t_ent **entity_list){
-    t_ent *prev_pnt = is_in_ent(id_ent, *entity_list);
-
-    if(prev_pnt != NULL){
-        t_ent *node_to_del;
-        t_ent *node_to_link;
-
-        if (is_ent_start == 1) {
-            node_to_del = prev_pnt;
-            node_to_link = prev_pnt->next;
-            *entity_list = node_to_link;
+    t_ent *result = is_in_ent(id_ent, *entity_list);
+    if(result != NULL){
+        t_ent *node_to_del = result;
+        t_ent *prev_node = node_to_del->prev;
+        t_ent *next_node = node_to_del->next;
+        if(prev_node == NULL){
+            *entity_list = next_node;
+            next_node->prev = NULL;
             free(node_to_del);
-        } else {
-            node_to_del = prev_pnt->next;
-            node_to_link = node_to_del->next;
-            prev_pnt->next = node_to_link;
+        } else if(next_node == NULL){
+            last_ent_node =  &((*last_ent_node)->prev);
+            prev_node->next = NULL;
+            free(node_to_del);
+        }else{
+            last_ent_node = &(prev_node->next);
+            prev_node->next = next_node;
+            next_node->prev = prev_node;
             free(node_to_del);
         }
-
-        //TODO: eliminare i link puntanti al nodo dagli altri nodi
     }
-
 }
 
-int add_rel(char *id_orig, char *id_dest, char *id_rel, t_ent **entity_list ){
+void add_rel(char *id_orig, char *id_dest, char *id_rel, t_ent **entity_list ){
     t_ent *dest_ent = is_in_ent(id_dest, *entity_list);
-
-    if(dest_ent != NULL){
-        if(is_ent_start != 1){
-            dest_ent = dest_ent->next;
-        }
-        t_rel *ent_rel_list = dest_ent->relation_type;
-
-        if(ent_rel_list == NULL){
-            t_rel *new_rel = (t_rel *)malloc(sizeof(t_rel));
+    t_ent *orig_ent = is_in_ent(id_orig, *entity_list);
+    if(dest_ent != NULL && orig_ent != NULL) {
+        t_rel *rel_list = dest_ent->rel_list;
+        t_rel *relation = is_in_rel(id_rel, rel_list);
+        if(relation == NULL && dest_ent->last_rel_node == NULL){
+            t_rel *new_rel = (t_rel *) malloc(sizeof(t_rel));
             new_rel->id_rel = id_rel;
-            new_rel->pointed_count = 1;
-            new_rel->ent = (t_rel_ent *)malloc(sizeof(t_rel_ent));
-            new_rel->ent->id_org = id_orig;
+            new_rel->counter = new_rel->counter + 1;
             new_rel->next = NULL;
+            new_rel->prev = NULL;
+            new_rel->orig_list = (t_orig *) malloc(sizeof(t_orig));
+            new_rel->orig_list->org_pnt = orig_ent;
+            new_rel->orig_list->next = NULL;
+            new_rel->orig_list->prev = NULL;
+            new_rel->last_orig_node = &(new_rel->orig_list);
+            dest_ent->rel_list = new_rel;
+            dest_ent->last_rel_node = &(dest_ent->rel_list);
+        }else if(relation == NULL && dest_ent->last_rel_node != NULL){
+            t_rel *new_rel = (t_rel *) malloc(sizeof(t_rel));
+            new_rel->id_rel = id_rel;
+            new_rel->counter = new_rel->counter + 1;
             new_rel->next = NULL;
-            dest_ent->relation_type = new_rel;
-        }else{
-            t_rel *relation = is_in_rel(id_rel, ent_rel_list);
-            if(is_rel_end == 0){
-                t_rel *new_rel = (t_rel *)malloc(sizeof(t_rel));
-                new_rel->id_rel = id_rel;
-                new_rel->pointed_count = 1;
-                new_rel->ent = (t_rel_ent *)malloc(sizeof(t_rel_ent));
-                new_rel->ent->id_org = id_orig;
-                new_rel->next = NULL;
-                new_rel->next = NULL;
-                relation->next = new_rel;
-            } else{
-                if(relation->ent == NULL){
-                    t_rel_ent *new_orig_ent = (t_rel_ent *) malloc(sizeof(t_rel_ent));
-                    new_orig_ent->id_org = id_orig;
-                    new_orig_ent->next = NULL;
-                    relation->ent = new_orig_ent;
-                } else {
-                    t_rel_ent *origin_ent = is_in_rel_ent(id_orig, relation->ent);
-                    if (is_rel_ent_end == 0) {
-                        t_rel_ent *new_orig_ent = (t_rel_ent *) malloc(sizeof(t_rel_ent));
-                        new_orig_ent->id_org = id_orig;
-                        new_orig_ent->next = NULL;
-                        origin_ent->next = new_orig_ent;
-                        relation->pointed_count = relation->pointed_count +1;
-                    }
-                }
+            new_rel->prev = *(dest_ent->last_rel_node);
+            new_rel->orig_list = (t_orig *) malloc(sizeof(t_orig));
+            new_rel->orig_list->org_pnt = orig_ent;
+            new_rel->orig_list->next = NULL;
+            new_rel->orig_list->prev = NULL;
+            new_rel->last_orig_node = &(new_rel->orig_list);
+            (*(dest_ent->last_rel_node))->next = new_rel;
+            dest_ent->last_rel_node = &((*(dest_ent->last_rel_node))->next);
+        } else if(relation != NULL){
+            t_orig *org_list = relation->orig_list;
+            t_orig *origin = is_in_origin(id_orig, org_list);
+            if (origin == NULL && relation->last_orig_node == NULL) {
+                t_orig *new_orig = (t_orig *) malloc(sizeof(t_orig));
+                new_orig->org_pnt = orig_ent;
+                new_orig->next = NULL;
+                new_orig->prev = NULL;
+                relation->orig_list = new_orig;
+                relation->last_orig_node = &(relation->orig_list);
+                relation->counter = relation->counter + 1;
+            } else if (origin == NULL && relation->last_orig_node != NULL) {
+                t_orig *new_orig = (t_orig *) malloc(sizeof(t_orig));
+                new_orig->org_pnt = orig_ent;
+                new_orig->next = NULL;
+                new_orig->prev = *(relation->last_orig_node);
+                ((*relation->last_orig_node))->next = new_orig;
+                relation->last_orig_node = &(((*relation->last_orig_node))->next);
+                relation->counter = relation->counter + 1;
             }
         }
     }
 }
+
 
 int del_rel(char *id_orig, char *id_dest, char *id_rel, t_ent **entity_list ){
-    t_ent *dest_ent = is_in_ent(id_dest, *entity_list);
 
-    if(dest_ent != NULL){
-        if(is_ent_start != 1){
-            dest_ent = dest_ent->next;
-        }
-        t_rel *ent_rel_list = dest_ent->relation_type;
-
-        if(ent_rel_list != NULL){
-            t_rel *relation = is_in_rel(id_rel, ent_rel_list);
-
-            if(relation != 0){
-                t_rel_ent * rel_ent_list = relation->ent;
-                if(rel_ent_list != NULL){
-                    t_rel_ent *orig_ent = is_in_rel_ent(id_dest, rel_ent_list);
-                    if(orig_ent != 0){
-                        relation->pointed_count = relation->pointed_count - 1;
-                        
-                    }
-                }
-            }
-        }
-    }
 }
 
 void report(){
@@ -242,17 +230,19 @@ void report(){
 
 int main() {
     t_ent *entity_list = NULL;
-    last_ent_node = &entity_list;
 
-    add_ent("Marco", &entity_list);
     add_ent("Giovanni", &entity_list);
     add_ent("Luca", &entity_list);
-    del_ent("Luca", &entity_list);
-    print_ent(entity_list);
-    printf("\n");
-    add_rel("Marco", "Giovanni", "amico_di", &entity_list);
-    add_rel("Luca", "Giovanni", "amico_di", &entity_list);
-    add_rel("Marco", "Giovanni", "collega_di", &entity_list);
-    print_all_rel("Giovanni", entity_list);
+    add_ent("Sofia", &entity_list);
+    add_ent("Nicola", &entity_list);
+    add_rel("Giovanni", "Luca", "amico_di", &entity_list);
+    add_rel("Sofia", "Luca", "amico_di", &entity_list);
+    add_rel("Giovanni", "Luca", "collega_di", &entity_list);
+    add_rel("Sofia", "Luca", "collega_di", &entity_list);
+    add_ent("Giuseppe", &entity_list);
+    add_rel("Giuseppe", "Giovanni", "amico_di", &entity_list);
 
+    print_ent(entity_list);
+    print_all_rel("Luca", entity_list);
+    print_all_rel("Giovanni", entity_list);
 }
